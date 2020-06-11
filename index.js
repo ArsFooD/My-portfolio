46a81e48933839939e396bf1925275915c693676
ArsFooD <66354404+ArsFooD@users.noreply.github.com>
2020-06-11 17:29:51 +0300

    переделываю
    Сейчас делаю вертикальные

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function Imgchange(){
-	document.getElementById("change").classList.toggle("img");
+function Bamboo(){
+	document.getElementById("bamboo").classList.remove("img");
+		document.getElementById("bamboo").classList.remove("bamboo");
+	document.getElementById("alu").classList.add("img");
+}
+function Alu(){
+	document.getElementById("alu").classList.remove("img");
+	document.getElementById("bamboo").classList.add("img");
 }
