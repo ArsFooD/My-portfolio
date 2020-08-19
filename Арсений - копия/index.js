61716d5d9498d766ed8fd5ae52a615e0c62398e7
ArsFooD <66354404+ArsFooD@users.noreply.github.com>
2020-08-19 03:29:31 +0300

    Обновил

---
+++ Арсений - копия/index.js
@@ -0,0 +1,15 @@
+var i=1;
+
+function name1(){
+    if (i==2) {
+        alert("Вы победили");
+    } 
+    if (i==3) {
+        alert("Вы проиграли");
+    }
+    else {
+        alert("а вы играли вообще?");
+    }
+}
+var image = document.getElementByClassName("thumbnail")
+new simpleParallax(image);
---
+++ Арсений - копия/slideMenu.js
@@ -0,0 +1,3 @@
+function color(){
+document.getElementById("nav-menu").classList.toggle("colorchange");
+}
